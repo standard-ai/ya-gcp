53dc602bf2cf52df3d871d5ec3523f44c6d38bf4
Joe Neeman <joeneeman@gmail.com>
2022-08-17 16:23:39 -0500

    Adds support for user account authentication.

--- examples/storage_get_object.rs
+++ examples/storage_get_object.rs
@@ -6,9 +6,18 @@ use ya_gcp::{AuthFlow, ClientBuilder, ClientBuilderConfig, ServiceAccountAuth};
 
 #[derive(Debug, StructOpt)]
 struct Args {
-    /// A path to the oauth service account key json file
+    /// A path to the oauth service account key json file.
+    ///
+    /// If this is not provided, `user_secrets` must be.
     #[structopt(long)]
-    service_account_key: std::path::PathBuf,
+    service_account_key: Option<std::path::PathBuf>,
+
+    /// Path to oauth user secrets, like those created by
+    /// `gcloud auth application-default login`
+    ///
+    /// If this is not provided, `service_account_key` must be.
+    #[structopt(long)]
+    user_secrets: Option<std::path::PathBuf>,
 
     /// The name of the bucket containing the desired object
     #[structopt(long)]
@@ -23,10 +32,15 @@ struct Args {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::from_args();
 
-    let builder = ClientBuilder::new(ClientBuilderConfig::new().auth_flow(
-        AuthFlow::ServiceAccount(ServiceAccountAuth::Path(args.service_account_key)),
-    ))
-    .await?;
+    let auth = if let Some(path) = args.service_account_key {
+        AuthFlow::ServiceAccount(ServiceAccountAuth::Path(path))
+    } else if let Some(path) = args.user_secrets {
+        AuthFlow::UserAccount(path)
+    } else {
+        panic!("at least one of service-account-key or user-secrets must be specified");
+    };
+
+    let builder = ClientBuilder::new(ClientBuilderConfig::new().auth_flow(auth)).await?;
 
     let client = builder.build_storage_client();
 
