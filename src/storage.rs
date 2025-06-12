896eca3356622781b082808ffe3069de5156d54e
Matt Neth <43559314+mjneth@users.noreply.github.com>
2025-06-12 13:53:52 -0500

    Fix cargo build issue

--- src/storage.rs
+++ src/storage.rs
@@ -93,7 +93,7 @@ fn bucket(name: &str) -> Result<BucketName, api::Error> {
         return Err(api::Error::InvalidSequence("google"));
     }
 
-    Ok(BucketName::non_validated(name.into()))
+    Ok(BucketName::non_validated(Box::leak(name.to_string().into_boxed_str())))
 }
 
 fn names_to_object<'a>(
