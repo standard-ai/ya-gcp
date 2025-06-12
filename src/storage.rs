a970659af88d0d43ede2f00363741b916fb6640a
Matt Neth <43559314+mjneth@users.noreply.github.com>
2025-06-12 14:06:54 -0500

    fmt

--- src/storage.rs
+++ src/storage.rs
@@ -93,7 +93,9 @@ fn bucket(name: &str) -> Result<BucketName, api::Error> {
         return Err(api::Error::InvalidSequence("google"));
     }
 
-    Ok(BucketName::non_validated(Box::leak(name.to_string().into_boxed_str())))
+    Ok(BucketName::non_validated(Box::leak(
+        name.to_string().into_boxed_str(),
+    )))
 }
 
 fn names_to_object<'a>(
